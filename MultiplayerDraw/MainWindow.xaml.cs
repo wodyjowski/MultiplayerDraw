0fdc578b79eb18c4286a26bf570bae3c2df25f3e
mkisiel <mkisiel@micomp.pl>
2019-04-16 08:57:44 +0200

    Add project files.

---
+++ MultiplayerDraw/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiplayerDraw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point StartPosition { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var mousePosition = e.GetPosition((Canvas)sender);
+                drawLine(mousePosition);
+            }
+        }
+
+
+        private void drawLine(Point mousePosition)
+        {
+            Line line = new Line();
+            //line.Width = 5;
+            line.X1 = StartPosition.X;
+            line.Y1 = StartPosition.Y;
+            line.X2 = mousePosition.X;
+            line.Y2 = mousePosition.Y;
+
+            line.Stroke = Brushes.Red;
+
+            StartPosition = mousePosition;
+
+            canvasDraw.Children.Add(line);
+        }
+
+        private void CanvasDraw_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StartPosition = e.GetPosition((Canvas)sender);
+        }
+
+        private void CanvasDraw_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Right)
+            {
+                canvasDraw.Children.Clear();
+            }
+        }
+    }
+}
